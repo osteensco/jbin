484d0d54f6f68d4620f631baead1068f26942cb1
br0steen <osteensco@gmail.com>
2024-06-23 08:38:58 -0500

    TODO

--- main.go
+++ main.go
@@ -12,6 +12,15 @@ func main() {
 
     fmt.Println("hello jbin")
 
+    // TODO
+    
+    // read JSON file key by key? or iteratively in some way
+
+    // convert to binary encoding
+    //      - keyLength|key|valueLength|value
+    //      - all values would be converted to UTF8 strings
+
+    // stream into a new file
 
 }
 
